b28606971f68355a40d4217e25b99d6560d1081e
ahnilhyuk <ilhyuk0504@gmail.com>
2025-07-22 22:57:37 +0900

    반응형 남음

--- script/index.js
+++ script/index.js
@@ -2,18 +2,23 @@ let swiperContainer = document.querySelector('.advertise_ct')
 let swiper = new Swiper('.advertise_ct', {
     slidesPerView: 1,
     spaceBetween: 0,
-    slidesPerGroup: 1, 
     loop: true,
+    effect: "fade",
     autoplay:{delay: 3500,},
-    centeredSlides : true,
+    
 })
 
 let swiperContainer01 = document.querySelector('.content_01')
 let swiper01 = new Swiper('.content_01', {
     slidesPerView: 1,
     spaceBetween: 0,
-    slidesPerGroup: 1, 
     loop: true,
     autoplay:{delay: 4000,},
-    centeredSlides : true,
+})
+
+let swiperContainer02 = document.querySelector('.best_box')
+let swiper02 = new Swiper('.best_box', {
+    slidesPerView: 4,
+    spaceBetween: 0,
+    loop: true,
 })
